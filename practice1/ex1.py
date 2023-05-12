7322cd34348cc1dcd7dd49420e390d55b3ca170b
xxbeann <yjb4945@gmail.com>
2023-05-12 19:22:38 +0900

    Mollu

--- practice1/ex1.py
+++ practice1/ex1.py
@@ -1,1 +1,29 @@
-print("hello world")
+p = ["leo", "kiki", "eden"]
+c = ["eden", "kiki"]
+
+pd = {}
+pc = {}
+answer = []
+
+for i in range(len(p)):
+       pd[p[i]] = p.count(p[i])
+for k in range(len(c)):
+       pc[c[k]] = c.count(c[k])
+
+print(pd)
+print(pc)
+a = list(pd.keys())
+b = list(pc.keys())
+print(a)
+
+if len(a) == len(b):
+       for j in range(len(a)):
+              if pd[a[j]] != pc[a[j]]:
+                     answer.append(a[j])
+
+else:
+       ans = set(a) - set(b)
+       answer = list(ans)
+
+print(answer)
+# 아,,,, 모르겠다,,,ㅜㅜ
