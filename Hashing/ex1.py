da942cd5e85a155f0ffe618a3a6d6627a759b2af
xxbeann <yjb4945@gmail.com>
2024-03-27 00:08:33 +0900

    txt_syscall_extraction

--- Hashing/ex1.py
+++ Hashing/ex1.py
@@ -1,15 +1,17 @@
-p = ["leo", "kiki", "eden"]
-c = ["eden", "kiki"]
+f = open("C:/Users/y_jeo/Desktop/strace.txt", 'rt', encoding='UTF-8')
+# line = f.readline()
+# print(line)
+list = []
 
-temp = 0
-dic = {}
-
-for part in p:
-       dic[hash(part)] = part
-       temp += int(hash(part))
-for com in c:
-       temp -= hash(com)
-print(dic)
-print(temp)
-print(dic[temp])
-# 키값이 해쉬함수 value값이 사람이름
+for line in f:
+  each = line.split()
+  if len(each) < 3: continue
+  target = each[2]
+  for i in range(len(target)):
+    p = -1
+    if target[i] == '(':
+      p = i
+      break
+  list.append(target[:p])
+  print(target[:p])
+print(set(list))
